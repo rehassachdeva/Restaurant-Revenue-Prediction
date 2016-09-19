1807136575abe39030234495344cbfa1bc10ed08
Rehas Sachdeva <aquannie@gmail.com>
2016-09-19 18:47:18 +0530

    Remove loc warning & add RMSE evaluation

--- src/randomForestRegressionSample.py
+++ src/randomForestRegressionSample.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import load_boston
 from sklearn.preprocessing import scale
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
 Boston = load_boston()
 
@@ -17,6 +19,6 @@ test = bosData[300:]
 rf = RandomForestRegressor()
 rf.fit(bosScaled[:300], Boston.target[:300])
 
-test['PREDICTION'] = rf.predict(bosScaled[300:])
+test.loc[:,'PREDICTION'] = rf.predict(bosScaled[300:])
 
-print test
+print sqrt(mean_squared_error(test['TARGET'], test['PREDICTION']))

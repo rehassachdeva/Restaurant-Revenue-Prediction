d16a8e5ebffd7a2e8361a95b4fdec75a06630df1
anjali.shenoy05@gmail.com <anjali.shenoy05@gmail.com>
2016-09-23 14:36:11 +0530

    Modify SVM with regression

--- src/svm_classify.py
+++ src/svm_classify.py
@@ -2,20 +2,21 @@
 #From Rehas
 import pandas as pd
 
-train = pd.read_csv("../data/trainIris.csv")
-test = pd.read_csv("../data/testIris.csv")
+train = pd.read_csv("../data/train.csv")
+#test = pd.read_csv("../data/testIris.csv")
+from sklearn.preprocessing import LabelEncoder  
 
 from sklearn import svm
-attributes = ['petal_length', 'petal_width', 'sepal_length', 'sepal_width']
-classes = ['class']
+attributes = ['P1','P2','P3','P4','P5','P6']
+classes = ['revenue']
 
 trainArr = train.as_matrix(attributes)
 trainClassArr = train.as_matrix(classes)
 
 
-#one vs many classifier
-lin_clf = svm.LinearSVC()
+#one vs many regresion classifier
+lin_clf = svm.LinearSVR()
 lin_clf.fit(trainArr, trainClassArr.ravel())	#ravel,to make numpy.ndarray to 1D
 
 #Predict
-print lin_clf.predict([[5.5, 1.8, 6.4, 3.1]])
+print lin_clf.predict([[6, 4.5,	6, 7.5, 6, 4]])

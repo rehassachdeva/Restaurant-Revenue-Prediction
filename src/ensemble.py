add76f6a7581c0726ea17ee668cfffe57f0ab082
Rehas Sachdeva <aquannie@gmail.com>
2016-09-24 09:46:11 +0530

    Integrate random forest, svm. Running code

--- src/ensemble.py
+++ src/ensemble.py
@@ -10,6 +10,14 @@ from datetime import datetime
 
 from sklearn.neighbors import KNeighborsClassifier
 
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.decomposition import PCA
+
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn import svm 
+
 def read_data(filename):
 	dataset = pd.read_csv(filename)
 	return dataset
@@ -55,18 +63,55 @@ def adjust_type(test_data):
 
 	return test_data
 
+def encode_transform(train_data, test_data, attributes):
+	train_arr = train_data.as_matrix(attributes)
+	test_arr = test_data.as_matrix(attributes)
+	M = train_arr
+	M = np.concatenate((M,test_arr), axis=0)
+
+	# To ensure test and train get the same encoding, also encode
+	# first 3 attributes
+	for i in range(3):
+		M[:, i] = LabelEncoder().fit_transform(M[:,i])
+
+	# Separate the train and test class
+	train_arr = M[:len(train_arr)]
+	test_arr = M[len(train_arr):]
+
+	return train_arr, test_arr
+
+def pca_transform(train_data, test_data):
+	pca = PCA(n_components=30)
+	pca_train_data = pca.fit_transform(train_data)
+	pca_test_data = pca.fit_transform(test_data)
+	return pca_train_data, pca_train_data
+
+def random_forest(train_arr, test_arr, train_class_arr):
+	rf = RandomForestRegressor()
+	rf.fit(train_arr, train_class_arr.ravel())
+	return rf.predict(test_arr)
+
+def svm_regressor(train_arr, test_data, train_class_arr):
+	# one vs many regresion classifier
+	lin_clf = svm.LinearSVR()
+	# ravel,to make numpy.ndarray to 1D
+	lin_clf.fit(train_arr, train_class_arr.ravel())
+	return lin_clf.predict(test_arr)
+
+def calc_final_predictions(rf_predictions, svm_predictions):
+	return [0.5 * (x + y) for x, y in zip(rf_predictions, svm_predictions)]
+
 if __name__ == "__main__":
 
 	train_data = read_data("train.csv")
 	test_data = read_data("test.csv")
 
-	train_fields = train_data.columns.values
-
-	attributes = train_fields[:-1]
-	target = [ train_fields[-1] ]
-
-	train_arr = train_data.as_matrix(attributes)
-	train_target_arr = train_data.as_matrix(target)
+	attributes = ['City', 'City Group', 'Type',
+			 'P1','P2','P3','P4','P5','P6',	'P7','P8','P9','P10','P11','P12',
+			 'P13','P14','P15','P16','P17','P18','P19','P20','P21','P22','P23',
+			 'P24','P25','P26','P27','P28','P29','P30','P31','P32','P33','P34',
+			 'P35','P36','P37', 'Days', 'Month', 'Year'
+	]
 
 	# x = list(train_data['revenue'])
 
@@ -75,13 +120,21 @@ if __name__ == "__main__":
 	# plot_histogram(x, "Revenue", "Frequency", "Histogram of Revenue")
 	# plot_histogram(y, "Log(Revenue)", "Frequency", "Histogram of Log(Revenue)")
 
-	# parse_date(test_data)
-
-	# print len(train_data.columns.values)
+	parse_date(train_data)
+	parse_date(test_data)
 
 	test_data = adjust_type(test_data)
 
-	print test_data
+	# train_data, test_data = pca_transform(train_data, test_data)
 
-	# print train_data
+	train_arr, test_arr = encode_transform(train_data, test_data, attributes)
 
+	train_class_arr = train_data.as_matrix(['revenue'])
+
+	rf_predictions = random_forest(train_arr, test_arr, train_class_arr)
+	
+	svm_predictions = svm_regressor(train_arr, test_arr, train_class_arr)
+
+	final_predictions = calc_final_predictions(rf_predictions, svm_predictions)
+
+	print final_predictions

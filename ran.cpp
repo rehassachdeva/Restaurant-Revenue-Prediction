639b0e74b1a4536e0fa874631fa4332833e533e2
anjali.shenoy05@gmail.com <anjali.shenoy05@gmail.com>
2016-10-03 02:47:13 +0530

    export files

---
+++ ran.cpp
@@ -0,0 +1,16 @@
+randomiser function
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# Explicit paths specified without -i or -o; assuming --only paths...
+# On branch master
+# Changes to be committed:
+#	new file:   ran.cpp
+#
+# Changes not staged for commit:
+#	modified:   .gitignore
+#
+# Untracked files:
+#	Team 38/
+#	src/lasso.py
+#	svr_trainer/
+#

de2cd962125768455cdece3cca41eb41cc26b3ff
anjali.shenoy05@gmail.com <anjali.shenoy05@gmail.com>
2016-10-09 20:12:59 +0530

    Lasso with methods

--- src/lasso.py
+++ src/lasso.py
@@ -15,4 +15,46 @@ class LogisticRegression:
         self.l1=l1
         self.l2=l2
         self.likelihood_history = []
+    def log_likelihood(self):
+
+        #Get Probablities
+        p = self.probability()
+        #Get Log Likelihood For Each Row of Dataset
+        loglikelihood = self.labels*np.log(p+1e-24) + (1-self.labels)*np.log(1-p+1e-24)
+        loglikelihood = -1*loglikelihood.sum()
+        #Return Sum
+        return loglikelihood
+ 
+    def probability(self):
+
+        return 1/(1+np.exp(-self.features.dot(self.w)))
+ 
+    def log_likelihood_gradient(self):
+
+        error = self.labels-self.probability()
+        product = error*self.features
+        grad = product.sum(axis=0).reshape(self.w.shape)
+        return grad
+ 
+    def gradient_decent(self,alpha=1e-7,max_iterations=1e4):
+
+        previous_likelihood = self.log_likelihood()
+        difference = self.tolerance+1
+        iteration = 0
+        self.likelihood_history = [previous_likelihood]
+        while (difference > self.tolerance) and (iteration < max_iterations):
+            self.w = self.w + alpha*self.log_likelihood_gradient()
+            temp = self.log_likelihood()
+            difference = np.abs(temp-previous_likelihood)
+            previous_likelihood = temp
+            self.likelihood_history.append(previous_likelihood)
+            iteration += 1
+ 
+    def predict_probabilty(self,X):
+        features = np.ones((X.shape[0],X.shape[1]+1))
+        features[:,1:] = (X-self.mean_x)/self.std_x
+        return 1/(1+np.exp(-features.dot(self.w)))
+ 
+    def get_coefficients(self):
+        return self.w.T[0]
  
